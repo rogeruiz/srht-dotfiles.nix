14868f783b80fdb59764c5aa3b902aedc69fd5a1
Roger Steve Ruiz <hi@rog.gr>
2024-04-16 09:33:37 -0500

    Actualizando mi Sketchybar

--- module/tools/sketchybar/config/plugins/clock.sh
+++ module/tools/sketchybar/config/plugins/clock.sh
@@ -9,4 +9,4 @@ meses_del_ano=("enero" "febrero" "marzo" "abril" "mayo" "junio" "julio" "agosto"
 meses_del_ano_corto=("enero" "feb." "marzo" "abr." "mayo" "jun." "jul." "agosto" "set." "oct." "nov." "dic.")
 
 # sketchybar --set $NAME label="$(date "+%d ${meses_del_ano_corto[$(date "+%m")]} '%y %H:%M:%S")"
-sketchybar --set $NAME label="$(date "+%y/%m/%d %H:%M")"
+sketchybar --set $NAME label="$(date "+%y/%m/%d %H:%M:%S")"
