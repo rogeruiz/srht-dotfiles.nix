423bc5c6f76c89fd8bb0092cd11a539d1f686372
Roger Steve Ruiz <hi@rog.gr>
2025-03-01 18:43:07 -0600

    Usa un variable pa' configurar el tipo de salida

--- module/tools/sketchybar/config/plugins/clock.sh
+++ module/tools/sketchybar/config/plugins/clock.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
 
+tipo="CORTO" # o `LARGO`
+
 dias_de_la_semana=(
   "domingo"
   "lunes"
@@ -68,14 +70,21 @@ meses_del_ano_abreviados=(
 
 # PERF: Aseguremos que los numeros de'l comando `date` son numeros usando
 # `$((..))` pa' convertir numeros con ceros delanteros en numeros sin ceros a
-# la izquierda.
+# la izquierda. Por ejemplo: Estos dos numeros no son iguales por el cero, 1 !=
+# 01, entonces lo cambio a un numero como 1 pa' que 1 == 1.
+#
+# INFO: Los numeros de los dias de la semana tienen rango de 0-7. Los numeros
+# de el mes tienen rango de 01-12.
+
 num_semana=$((10#$(date +%w) + 0))
 num_mes=$((10#$(date +%m) - 1))
 
-# dia="${dias_de_la_semana[${num_semana}]}"
-# mes="${meses_del_ano[${num_mes}]}"
-dia="${dias_de_la_semana_abreviados[${num_semana}]}"
-mes="${meses_del_ano_abreviados[${num_mes}]}"
+if [ "${tipo}" == "CORTO" ]; then
+  dia="${dias_de_la_semana_abreviados[${num_semana}]}"
+  mes="${meses_del_ano_abreviados[${num_mes}]}"
+elif [ "LARGO" == "${tipo}" ]; then
+  dia="${dias_de_la_semana[${num_semana}]}"
+  mes="${meses_del_ano[${num_mes}]}"
+fi
 
 sketchybar --set "${NAME:=clock}" label="$(date "+${dia} %d ${mes} '%y %H:%M:%S")"
-# sketchybar --set "${NAME:=clock}" label="$(date "+%y/%m/%d %H:%M:%S")"
